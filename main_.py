79cec7b7c5a63ac278dd86b31070f2a5bb09d0e8
OlegBurov88 <ovburov88@gmail.com>
2024-10-12 18:15:11 +0500

    Лекция. Пакеты и способы импорта.

---
+++ main_.py
@@ -0,0 +1,6 @@
+from package1.package2.package3.module3 import good_word as m3
+from package1.package2.package3.module4 import good_word as m4
+
+if __name__ == '__main__':
+    m3('Олег')
+    m4('Владимир')
