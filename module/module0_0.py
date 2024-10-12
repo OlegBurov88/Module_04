ba043a1940ac355f0ce411e785cbd4f38812d0a8
OlegBurov88 <ovburov88@gmail.com>
2024-10-12 12:52:30 +0500

    Лекция. Модули и пакеты

---
+++ module/module0_0.py
@@ -0,0 +1,7 @@
+import module3
+print(dir(module3))
+print('Привет я из главного модуля')
+
+print(module3.a, module3.b)
+
+module3.say_hi()
---
+++ module/module0_1.py
@@ -0,0 +1,7 @@
+import module3 as m3
+print(dir(m3))
+print('Привет я из главного модуля')
+
+print(m3.a, m3.b)
+
+m3.say_hi()
